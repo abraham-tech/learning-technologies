2ff027e9c031d336b4d984c7c00d40f1ac0f6813
Abraham <abraham.mathewos.meja@gmail.com>
2021-01-14 21:04:32 +0100

    setup models and migrations

---
+++ graphql-recipe-server/migrations/20210114190744-create-recipe.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Recipes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      ingredients: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      direction: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      userId: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        allowNull: false
+
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Recipes');
+  }
+};
