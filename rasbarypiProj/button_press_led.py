a0c56c8e64e2328fa833d1a81a072ad2c6a82435
Abraham <abraham.mathewos.meja@gmail.com>
2021-01-19 21:30:44 +0100

    button and led status

---
+++ rasbarypiProj/button_press_led.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time 
+
+GPIO.setwarning(False)
+
+ledPid = 11
+buttonPin = 16
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(ledPin GPIO.OUT)
+GPIO.output(ledPin, GPIO.LOW)
+
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True: 
+    GPIO.wait_for_the_edge(buttonPin, GPIO.FALLING)
+    print("Button Pressed")
+    GPIO.output(ledPin, GPIO.HIGH)
+    GPIO.wait_for_the_edge(buttonPin, GPIO.RISING)
+    GPIO.output(ledPin, GPIO.LOW)
+
+GPIO.cleanup()
