322ceff18d5458a7d3c63adf431243d385158b6a
Abraham <abraham.mathewos.meja@gmail.com>
2021-02-16 09:19:09 +0100

    testing

--- server/api/src/index.js
+++ server/api/src/index.js
@@ -109,12 +109,12 @@ app.get('/reset',function(req,res){
 app.get("/test_sensors", function(request, response) {
   response.setHeader("Content-Type", "application/json");
   response.header("Access-Control-Allow-Origin", "*");
-  const pyProg = spawn('python3', ['/home/pi/Gronska/schedule/scripts/test_sensors.py']);
+  const pyProg = spawn('python3', ['/home/pi/Gronska/schedule/test_sensors.py'],{cwd:'/home/pi/Gronska/schedule'});
   pyProg.stdout.on('data', function(data) {
       console.log(data.toString());
       response.send({
         status: "success",
-        email: "Growing light turned off"
+        message: data.toString()
       });
   });
 });
@@ -123,12 +123,12 @@ app.get("/test_sensors", function(request, response) {
 app.get("/test_basement", function(request, response) {
   response.setHeader("Content-Type", "application/json");
   response.header("Access-Control-Allow-Origin", "*");
-  const pyProg = spawn('python3', ['/home/pi/Gronska/schedule/scripts/test_basement.py']);
+  const pyProg = spawn('python3', ['/home/pi/Gronska/schedule/test_basement.py'],{cwd:'/home/pi/Gronska/schedule'});
   pyProg.stdout.on('data', function(data) {
       console.log(data.toString());
       response.send({
         status: "success",
-        email: "Growing light turned off"
+        message: data.toString()
       });
   });
 });
@@ -137,12 +137,12 @@ app.get("/test_basement", function(request, response) {
 app.get("/test_floors", function(request, response) {
   response.setHeader("Content-Type", "application/json");
   response.header("Access-Control-Allow-Origin", "*");
-  const pyProg = spawn('python3', ['/home/pi/Gronska/schedule/scripts/test_floors.py']);
+  const pyProg = spawn('python3', ['/home/pi/Gronska/schedule/test_floors.py'],{cwd:'/home/pi/Gronska/schedule'});
   pyProg.stdout.on('data', function(data) {
       console.log(data.toString());
       response.send({
         status: "success",
-        email: "Growing light turned off"
+        message: data.toString()
       });
   });
 });
@@ -151,26 +151,26 @@ app.get("/test_floors", function(request, response) {
 app.get("/test_main_pump", function(request, response) {
   response.setHeader("Content-Type", "application/json");
   response.header("Access-Control-Allow-Origin", "*");
-  const pyProg = spawn('python3', ['/home/pi/Gronska/schedule/scripts/test_main_pump.py']);
+  const pyProg = spawn('python3', ['/home/pi/Gronska/schedule/test_main_pump.py'],{cwd:'/home/pi/Gronska/schedule'});
   pyProg.stdout.on('data', function(data) {
       console.log(data.toString());
       response.send({
         status: "success",
-        email: "Growing light turned off"
+        message: data.toString()
       });
   });
 });
 
 
-app.get("/read_temperature", function(request, response) {
+app.get("/test_read_temperature", function(request, response) {
   response.setHeader("Content-Type", "application/json");
   response.header("Access-Control-Allow-Origin", "*");
-  const pyProg = spawn('python3', ['/home/pi/Gronska/schedule/scripts/read_temperature.py']);
+  const pyProg = spawn('python3', ['/home/pi/Gronska/schedule/test_read_temperature.py'],{cwd:'/home/pi/Gronska/schedule'});
   pyProg.stdout.on('data', function(data) {
       console.log(data.toString());
       response.send({
         status: "success",
-        email: "Growing light turned off"
+        message: data.toString()
       });
   });
 });
@@ -179,12 +179,25 @@ app.get("/read_temperature", function(request, response) {
 app.get("/test_basement_door", function(request, response) {
   response.setHeader("Content-Type", "application/json");
   response.header("Access-Control-Allow-Origin", "*");
-  const pyProg = spawn('python3', ['/home/pi/Gronska/schedule/scripts/test_basement_door.py']);
+  const pyProg = spawn('python3', ['/home/pi/Gronska/schedule/test_basement_door.py'],{cwd:'/home/pi/Gronska/schedule'});
   pyProg.stdout.on('data', function(data) {
       console.log(data.toString());
       response.send({
         status: "success",
-        email: "Growing light turned off"
+        message: data.toString()
+      });
+  });
+});
+
+app.get("/test_reset_boards", function(request, response) {
+  response.setHeader("Content-Type", "application/json");
+  response.header("Access-Control-Allow-Origin", "*");
+  const pyProg = spawn('python3', ['/home/pi/Gronska/test_reset_boards.py'],{cwd:'/home/pi/Gronska/schedule'});
+  pyProg.stdout.on('data', function(data) {
+      console.log(data.toString());
+      response.send({
+        status: "success",
+        message: data.toString()
       });
   });
 });
