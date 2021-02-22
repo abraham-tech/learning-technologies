06270022f18fc04f1fc531efaeae21a64de00934
Abraham <abraham.mathewos.meja@gmail.com>
2021-02-22 22:46:59 +0100

    hn

--- hackernews-clone-react-app/src/router/AppRouter.js
+++ hackernews-clone-react-app/src/router/AppRouter.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Router, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header';
 import HomePage from '../components/HomePage';
 import PageNotFound from '../components/PageNotFound';
+import ShowStories from '../components/ShowStories';
 
 const AppRouter = () => {
-
-    return (
-        <BrowserRouter>
-            <div>
-                <Header/>
-                <Switch>
-                    <Route path='/' component={HomePage} exact='true'/>
-                    <Route component={PageNotFound} />
-                </Switch>
-            </div>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route path="/" component={HomePage} exact={true} />
+          <Route path="/:type" component={ShowStories} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default AppRouter;
