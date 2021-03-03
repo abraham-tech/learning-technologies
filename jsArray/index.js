0fb401ffc717b256df091a2a295d8098ca30a8b5
Abraham <abraham.mathewos.meja@gmail.com>
2021-03-03 15:37:37 +0100

    filtering array of objects

--- jsArray/index.js
+++ jsArray/index.js
@@ -14,5 +14,23 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June','July', '
 // console.log(data)
 
 //find index of
-const index = months.findIndex(month => month == "April")
-console.log("Index ", index)
+// const index = months.findIndex(month => month == "April")
+// console.log("Index ", index)
+
+//array filter months
+
+filtered = months.filter(month => month == 'May')
+console.log(filtered)
+
+const employees = [
+    { name: 'David Carlson', age: 30 },
+    { name: 'John Cena', age: 34 },
+    { name: 'Mike Sheridan', age: 25 },
+    { name: 'John Carte', age: 50 }
+  ];
+
+//get employees age greater than 300
+
+const filteredEmployees = employees.filter(employee => employee.age > 30)
+
+console.log(filteredEmployees)
