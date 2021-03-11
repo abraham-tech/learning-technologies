74507e82b9e8adada84fb8f7ec932b7a49efcbae
Abraham <abraham.mathewos.meja@gmail.com>
2021-03-11 10:25:09 +0100

    flask api practive

--- flask-api/application.py
+++ flask-api/application.py
@@ -17,3 +17,30 @@ class Artist(db.Model):
 
 #Create the table for the artists table 
 db.create_all()
+
+
+#Create resource management end points for
+
+from flask_rest_jsonapi import Api, ResourceDetail, ResourceList 
+
+class ArtistMany (ResourceList):
+    schema = ArtistSchema
+    data_layer = {
+        'session': db.session,
+        'model': Artist
+    }
+
+class ArtistOne(ResourceDetail):
+    schema = ArtistSchema
+    data_layer = {
+        'session': db.session,
+        'model': Artist
+    }
+
+api = Api(app)
+api.route(ArtistMany, 'artist_many', '/artists')
+api.route(ArtistOne, 'artist_one', '/artists/<int:id>')
+
+# main loop to run app in debug mode
+if __name__=='__main__':
+    app.run(debug=True)
